ecb5f6fd4a7a36e4ed0346e180826bdbc1bd8d64
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2020-04-17 22:46:38 +0900

    Update lp content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgLpModule } from '@deer-inc/ng-lp';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgLpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
